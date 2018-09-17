46537f3ccb5734bef642bad172c4cb14528b2462
Vegard Bjørgan <vegbjoe@gmail.com>
2018-09-17 13:44:04 +0200

    Code for separate standardscaling

--- PCA.py
+++ PCA.py
@@ -1,20 +1,5 @@
 import pandas as pd
-# This is sample code
-'''
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
-# load dataset into Pandas DataFrame
-
-df = pd.read_csv(url, names=['sepal length','sepal width','petal length','petal width','target'])
-
-
-from sklearn.preprocessing import StandardScaler
-features = ['sepal length', 'sepal width', 'petal length', 'petal width']
-# Separating out the features
-x = df.loc[:, features].values
-# Separating out the target
-y = df.loc[:,['target']].values
-'''
-
+import numpy as np
 
 import Data_Reader
 df, target = Data_Reader.Read_Hepmark_Microarray()
@@ -25,17 +10,34 @@ features = df.axes[1].values
 df['target'] = target
 x = df.loc[:,features].values
 y = df.loc[:,'target'].values #df.axes[0].values
+idx = []
+last = df.axes[0][0]
+for i,index in enumerate(df.axes[0]):
+    if index[:3] != last[:3]:
+        idx.append(i)
+        last = index
+idx.append(len(x))
+
 
 from sklearn.preprocessing import StandardScaler
 # Standardizing the features
-x = StandardScaler().fit_transform(x)
+xs = []
+last = 0
+for i in idx:
+    xs.append(StandardScaler().fit_transform(x[last:i]))
+    last = i
+
+x = np.concatenate((xs), axis=0)
+
+
+df_index = df.axes[0]
 
 from sklearn.decomposition import PCA
 pca = PCA(n_components=2)
 principalComponents = pca.fit_transform(x)
 principalDf = pd.DataFrame(data = principalComponents
              , columns = ['principal component 1', 'principal component 2']
-             , index = df.axes[0])
+             , index = df_index)
 
 finalDf = pd.concat([principalDf, df[['target']]], axis = 1)
 #pd.concat([principalDf, df.axes[0]], axis = 1)
@@ -47,7 +49,7 @@ fig = plt.figure(figsize = (8,8))
 ax = fig.add_subplot(1,1,1)
 ax.set_xlabel('Principal Component 1', fontsize = 15)
 ax.set_ylabel('Principal Component 2', fontsize = 15)
-ax.set_title('2 component PCA', fontsize = 20)
+ax.set_title('2 component PCA Hepmark-Microarray', fontsize = 20)
 targets = ['Normal', 'Tumor', 'Undefined'] #['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
 colors = ['r', 'g', 'b']
 for target, color in zip(targets,colors):
@@ -62,26 +64,3 @@ ax.grid()
 pca.explained_variance_ratio_
 
 plt.show()
-
-'''
-
-from sklearn.model_selection import train_test_split
-# test_size: what proportion of original data is used for test set
-train_img, test_img, train_lbl, test_lbl = train_test_split( mnist.data, mnist.target, test_size=1/7.0, random_state=0)
-
-
-from sklearn.preprocessing import StandardScaler
-scaler = StandardScaler()
-# Fit on training set only.
-scaler.fit(train_img)
-# Apply transform to both the training set and the test set.
-train_img = scaler.transform(train_img)
-test_img = scaler.transform(test_img)
-
-from sklearn.decomposition import PCA
-# Make an instance of the Model
-pca = PCA(.95)
-
-pca.fit(train_img)
-
-'''
