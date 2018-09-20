7c837449be7e693e5d0c5dc13c34d48d2fa3462a
Vegard Bjørgan <vegbjoe@gmail.com>
2018-09-20 11:57:39 +0200

    pca utils init

---
+++ pca_utils.py
@@ -0,0 +1,13 @@
+'''
+Vegard Bjørgan 2018
+
+pca_utils.py contains utilities for pca.py
+'''
+
+def transform_sequence_to_microarray(df):
+    pass
+    # Add 0.5
+    # For each cell in column v_ij * 10^6 / col_sum
+    # cell = log_2 cell
+
+    return df
