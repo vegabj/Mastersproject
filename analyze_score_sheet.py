b46d346102aa52ddfc5674e7d60e74b18b8f0451
Vegard Bjørgan <vegbjoe@gmail.com>
2018-11-07 15:11:09 +0100

    heatmap changes

--- analyze_score_sheet.py
+++ analyze_score_sheet.py
@@ -14,11 +14,16 @@ df = pd.read_csv(path, index_col = 0)
 
 test_sizes = ['0', '1', '2', '4', '8', '16', 'all']
 
-overall_nn = []
+score_dict = {"overall": [], "none": [], "standard": [], "other": []}
 for P in test_sizes:
     overall_n = []
+    none_n = []
+    standard_n = []
+    other_n = []
     for N in test_sizes:
         select = df.loc[(df["P"] == P) & (df["N"] == N)]
+        # Hepmark_Microarray  Hepmark_Paired_Tissue
+        #select = select.loc[select["Dataset"] == "Hepmark_Paired_Tissue"]
         select_none = select.loc[df["Normalization"] == 'None']
         select_standard = select.loc[df["Normalization"] == 'Standard']
         select_other = select.loc[df["Normalization"] == 'Other']
@@ -29,24 +34,34 @@ for P in test_sizes:
         print("\nP", P, "N", N)
         print("Overall:", overall, "None", none, "Standard", standard, "Other", other)
         overall_n.append(overall)
-    overall_nn.append(overall_n)
+        none_n.append(none)
+        standard_n.append(standard)
+        other_n.append(other)
+    score_dict["overall"].append(overall_n)
+    score_dict["none"].append(none_n)
+    score_dict["standard"].append(standard_n)
+    score_dict["other"].append(other_n)
 
 test_sizes_p = [x+"P" for x in test_sizes]
 test_sizes_n = [x+"N" for x in test_sizes]
-# Heatmap example
+ds = [score_dict["overall"], score_dict["none"], score_dict["standard"], score_dict["other"]]
+
+# Heatmap
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import heatmap
-overall_nn = np.array(overall_nn)
-fig, ax = plt.subplots()
-im, cbar = heatmap.heatmap(overall_nn, test_sizes_p, test_sizes_n, ax=ax,
-                   cmap=cm.coolwarm, cbarlabel="score [AUC / Sp]")
-texts = heatmap.annotate_heatmap(im, valfmt="{x:.1f}")
 
-fig.tight_layout()
-plt.show()
+for d in ds:
+    scores = np.array(d)
+    fig, ax = plt.subplots()
+    im, cbar = heatmap.heatmap(scores, test_sizes_p, test_sizes_n, ax=ax,
+                       cmap=cm.coolwarm, cbarlabel="score [AUC / Sp]")
+    texts = heatmap.annotate_heatmap(im, valfmt="{x:.1f}")
+
+    fig.tight_layout()
+    plt.show()
 
 
-# 3D plot example
+# 3D plot
 from plot_3d import plot_3d
-plot_3d(overall_nn, test_sizes_p, test_sizes_n)
+plot_3d(np.array(score_dict["overall"]), test_sizes_p, test_sizes_n)
